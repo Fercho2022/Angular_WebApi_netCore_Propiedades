d178313f6c83d1149a295dc599e00b0e18c03a12
Fercho2022 <crosio.fernando@gmail.com>
2024-11-15 16:34:25 -0300

    se reaiza el formulario reactivo del register con sus validaciones

--- src/app/Components/user/user-register/user-register.component.ts
+++ src/app/Components/user/user-register/user-register.component.ts
@@ -1,22 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-register.component.html',
   styleUrl: './user-register.component.css'
 })
 export class UserRegisterComponent implements OnInit{
 
-  registrationFrom!: FormGroup;
+registrationForm!: FormGroup;
+
+private formBuilder=inject(FormBuilder);
+
+ngOnInit(): void {
+    this.registrationForm= this.formBuilder.group({
+      userName:['', [Validators.required, Validators.minLength(5)]],
+      password:['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword:['', [Validators.required]],
+      mobile:['', [Validators.required]],
+      email:['', [Validators.required, Validators.email]]
 
 
+    }, {validators: this.passwordMatchingValidator});
 
-  ngOnInit(): void {
+    }
 
+passwordMatchingValidator(fg:FormGroup):ValidationErrors | null{
+
+    const password = fg.get('password')?.value;
+    const confirmPassword = fg.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { notmatched: true };
+  }
+
+onSubmit(){
+  console.log( this.registrationForm)
+    }
+
+// Getter methods for all form controls
+get userName(){
+  return this.registrationForm.get('userName') as FormControl;
+}
+
+get email(){
+  return this.registrationForm.get('email') as FormControl;
+}
+
+get password(){
+  return this.registrationForm.get('password') as FormControl;
+}
+
+get confirmPassword(){
+  return this.registrationForm.get('confirmPassword') as FormControl;
+}
+
+get mobile(){
+  return this.registrationForm.get('mobile') as FormControl;
+}
   }
 
 
-}
+
