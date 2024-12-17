0979ff3ecb1a0a704a08c23c24cd06ea6cfa1e58
Fercho2022 <crosio.fernando@gmail.com>
2024-12-17 12:59:29 -0300

    agrega propertie segun si es de compra o alquiler

--- src/app/Interfaces/IPropertyBase.ts
+++ src/app/Interfaces/IPropertyBase.ts
@@ -8,11 +8,7 @@ export interface IPropertyBase{
   BHK: number | null;
   BuiltArea: number | null;
   City: string;
-<<<<<<< HEAD
-  RTM:string;
-=======
   RTM:boolean | null;
   PossesionOn:string ;
->>>>>>> de46aa05b03a90b16a02e067c2b1f7c63000b85b
   Image?:string;
 }
