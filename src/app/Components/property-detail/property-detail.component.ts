768025c4a2ff5940385884a23664f9bd9d396827
Fercho2022 <crosio.fernando@gmail.com>
2024-12-26 19:39:56 -0300

    agregado de galeria de fotos dentro del Tab de property-detail

--- src/app/Components/property-detail/property-detail.component.ts
+++ src/app/Components/property-detail/property-detail.component.ts
@@ -5,17 +5,21 @@ import { HousingService } from '../../Services/housing.service';
 import { Property } from '../../Interfaces/property';
 import { IPropertyBase } from '../../Interfaces/IPropertyBase';
 import { IProperty } from '../../Interfaces/IProperty';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryModule, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+
 
 @Component({
   selector: 'app-property-detail',
   templateUrl: './property-detail.component.html',
   styleUrls: ['./property-detail.component.css'],
   standalone: true,
-  imports: [RouterModule, TabsModule],
+  imports: [RouterModule, TabsModule, NgxGalleryModule],
 })
 export class PropertyDetailComponent implements OnInit {
   propertyId = signal<number>(0); // Inicializamos signal con un valor por defecto
   property = new Property();
+  galleryOptions!: NgxGalleryOptions[];
+  galleryImages!: NgxGalleryImage[];
 
   constructor(
     private route: ActivatedRoute,
@@ -30,5 +34,48 @@ export class PropertyDetailComponent implements OnInit {
         this.propertyId.set(this.property.Id);
       }
     });
+
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '465px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+
+      },
+
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/Images/internal-1.jpg',
+        medium: 'assets/Images/internal-1.jpg',
+        big: 'assets/Images/internal-1.jpg'
+      },
+      {
+        small: 'assets/Images/internal-2.jpg',
+        medium: 'assets/Images/internal-2.jpg',
+        big: 'assets/Images/internal-2.jpg'
+      },
+      {
+        small: 'assets/Images/internal-3.jpg',
+        medium: 'assets/Images/internal-3.jpg',
+        big: 'assets/Images/internal-3.jpg'
+      },
+      {
+        small: 'assets/Images/internal-4.jpg',
+        medium: 'assets/Images/internal-4.jpg',
+        big: 'assets/Images/internal-4.jpg'
+      },
+      {
+        small: 'assets/Images/internal-5.jpg',
+        medium: 'assets/Images/internal-5.jpg',
+        big: 'assets/Images/internal-5.jpg'
+      },
+
+    ];
   }
+
+
 }
+
